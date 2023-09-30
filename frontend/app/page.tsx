a4b68fc78ce288a6a822296328737fa910568464
Jordan Rollins <carpeomniavino@gmail.com>
2023-09-30 21:16:34 +0200

    updated visuals

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,11 +2,30 @@
 import InstructionsComponent from "@/components/instructionsComponent";
 import styles from "./page.module.css";
 import "./globals.css";
+import ComboBox from '../components/ComboBox'
+
+
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <InstructionsComponent></InstructionsComponent>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '30px',
+          flexDirection: 'column',
+        }}
+      >
+        <img
+          src='/header.png'
+          alt='SmartConSwap'
+          style={{ width: 'auto', height: '100px', marginBottom: '100px' }}
+        />
+      </div>
+
+      <ComboBox></ComboBox>
     </main>
   );
 }
--- frontend/components/ComboBox.tsx
+++ frontend/components/ComboBox.tsx
@@ -15,79 +15,85 @@ function Dropdown({ options, defaultDisplay }) {
   }, [searchTerm]);
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        padding: '16px',
-        borderRadius: '1rem',
-        position: 'relative',
-        width: '256px',
-        marginTop: '16px',
-      }}
-    >
+    <>
       <div
         style={{
-          width: '100%',
-          padding: '8px',
-          //   border: '1px solid #cbd5e0',
-          borderRadius: '5px',
-          cursor: 'pointer',
           display: 'flex',
-          justifyContent: 'space-between',
+          flexDirection: 'column',
           alignItems: 'center',
-          backgroundColor: '#352f44',
-          color: 'white',
+          padding: '16px',
+          borderRadius: '1rem',
+          position: 'relative',
+          width: '256px',
+          marginTop: '16px',
         }}
-        onClick={() => setShowDropdown(!showDropdown)}
       >
-        <span>{displayValue}</span>
-        <span>▼</span>
-      </div>
-      {showDropdown && (
         <div
           style={{
-            position: 'absolute',
-            zIndex: 10,
-            width: '100%',
-            marginTop: '8px',
+            width: '200px',
+            padding: '8px',
+            //  border: '1px solid #cbd5e0',
+            borderRadius: '5px',
+            cursor: 'pointer',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
             backgroundColor: '#352f44',
             color: 'white',
-            border: '1px solid #cbd5e0',
-            borderRadius: '1rem',
-                      boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-            padding: '16px',
+            fontFamily: 'sans-serif',
+            boxShadow:
+              'rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset',
           }}
+          onClick={() => setShowDropdown(!showDropdown)}
         >
-          <input
+          <span>{displayValue}</span>
+          <span>▼</span>
+        </div>
+        {showDropdown && (
+          <div
+            style={{
+              position: 'absolute',
+              zIndex: 10,
+              width: '100%',
+              marginTop: '8px',
+              backgroundColor: '#352f44',
+              color: 'white',
+              border: '1px solid #cbd5e0',
+              borderRadius: '5px',
+              boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+              padding: '16px',
+              fontFamily: 'sans-serif',
+            }}
+          >
+            {/* <input
             type='text'
             style={{ width: '100%', padding: '8px', borderRadius: '1rem' }}
             placeholder='Search...'
             onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          {filteredOptions.map((option) => (
-            <div
-              key={option.value}
-              style={{ padding: '8px', cursor: 'pointer' }}
-              onClick={() => {
-                setDisplayValue(option.label);
-                setShowDropdown(false);
-                setSearchTerm('');
-              }}
-              onMouseOver={(e) =>
-                (e.currentTarget.style.backgroundColor = '#edf2f7')
-              }
-              onMouseOut={(e) =>
-                (e.currentTarget.style.backgroundColor = 'transparent')
-              }
-            >
-              {option.label}
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+          /> */}
+            {filteredOptions.map((option) => (
+              <div
+                key={option.value}
+                style={{ padding: '8px', cursor: 'pointer' }}
+                onClick={() => {
+                  setDisplayValue(option.label);
+                  setShowDropdown(false);
+                  setSearchTerm('');
+                }}
+                onMouseOver={(e) =>
+                  (e.currentTarget.style.backgroundColor = 'transparent')
+                }
+                onMouseOut={(e) =>
+                  (e.currentTarget.style.backgroundColor = 'transparent')
+                }
+              >
+                {option.label}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
@@ -111,27 +117,140 @@ export default function Home() {
 
   const [numberInputOne, setNumberInputOne] = useState('0');
   const [numberInputTwo, setNumberInputTwo] = useState('0');
+  const [numberInputThree, setNumberInputThree] = useState('0');
+  const [numberInputFour, setNumberInputFour] = useState('0');
 
   return (
     <>
-      <h1 style={{ marginLeft: '250px', fontSize: '24px' }}>From</h1>
+      <h1
+        style={{
+          marginLeft: '250px',
+          fontSize: '24px',
+          fontFamily: 'sans-serif',
+        }}
+      >
+        From
+      </h1>
       <div
         style={{
           display: 'flex',
           flexDirection: 'row',
-          backgroundColor: 'black',
+          backgroundColor: 'white',
           padding: '16px',
           justifyContent: 'space-around',
           alignItems: 'center',
           marginLeft: '250px',
           marginRight: '250px',
           borderRadius: '1rem',
+          fontFamily: 'sans-serif',
+          boxShadow:
+            'rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset',
         }}
       >
         <div
           style={{
             borderRadius: '1rem',
-            backgroundColor: 'black',
+            backgroundColor: 'white',
+            //  border: '1px solid blue',
+          }}
+        >
+          <Dropdown options={optionsOne} defaultDisplay='Select Chain' />
+          <div style={{ height: '55px' }}></div>
+        </div>
+        <div
+          style={{
+            borderRadius: '1rem',
+            backgroundColor: 'white',
+            // border: '1px solid blue',
+          }}
+        >
+          <Dropdown options={optionsTwo} defaultDisplay='Select Token' />
+          <input
+            type='text'
+            inputMode='decimal'
+            value={numberInputOne}
+            onChange={(e) => {
+              const value = e.target.value;
+              // Regular expression to allow numbers and decimals
+              if (/^\d*\.?\d*$/.test(value) || value === '')
+                setNumberInputOne(value);
+            }}
+            style={{
+              width: '40%',
+              padding: '8px',
+              color: 'black',
+              backgroundColor: 'white',
+              fontSize: '24px',
+              fontWeight: 'bold',
+              textAlign: 'center',
+              marginLeft: '25px',
+              fontFamily: 'sans-serif',
+            }}
+            defaultValue={0}
+          />
+        </div>
+        <div
+          style={{
+            borderRadius: '1rem',
+            backgroundColor: 'white',
+            // border: '1px solid blue',
+          }}
+        >
+          <Dropdown options={optionsThree} defaultDisplay='Select Token' />
+          <input
+            type='text'
+            inputMode='decimal'
+            value={numberInputTwo}
+            onChange={(e) => {
+              const value = e.target.value;
+              // Regular expression to allow numbers and decimals
+              if (/^\d*\.?\d*$/.test(value) || value === '')
+                setNumberInputTwo(value);
+            }}
+            style={{
+              width: '40%',
+              padding: '8px',
+              color: 'black',
+              backgroundColor: 'white',
+              fontSize: '24px',
+              fontWeight: 'bold',
+              textAlign: 'center',
+              marginLeft: '25px',
+              fontFamily: 'sans-serif',
+            }}
+            defaultValue={0}
+          />
+        </div>
+      </div>
+      <div style={{ height: '50px' }}></div>
+      <h1
+        style={{
+          marginLeft: '250px',
+          fontSize: '24px',
+          fontFamily: 'sans-serif',
+        }}
+      >
+        To
+      </h1>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          backgroundColor: 'white',
+          padding: '16px',
+          justifyContent: 'space-around',
+          alignItems: 'center',
+          marginLeft: '250px',
+          marginRight: '250px',
+          borderRadius: '1rem',
+          boxShadow:
+            'rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset',
+        }}
+      >
+        <div
+          style={{
+            borderRadius: '1rem',
+            backgroundColor: 'white',
             // border: '1px solid blue',
           }}
         >
@@ -141,147 +260,97 @@ export default function Home() {
         <div
           style={{
             borderRadius: '1rem',
-            backgroundColor: 'black',
+            backgroundColor: 'white',
             // border: '1px solid blue',
           }}
         >
           <Dropdown options={optionsTwo} defaultDisplay='Select Token' />
           <input
-            type='number'
-            value={numberInputOne}
+            type='text'
+            inputMode='decimal'
+            value={numberInputThree}
             onChange={(e) => {
               const value = e.target.value;
-              if (value >= 0) setNumberInputOne(value);
+              // Regular expression to allow numbers and decimals
+              if (/^\d*\.?\d*$/.test(value) || value === '')
+                setNumberInputThree(value);
             }}
             style={{
-              width: '15%',
+              width: '40%',
               padding: '8px',
-              color: 'white',
-              backgroundColor: 'black',
+              color: 'black',
+              backgroundColor: 'white',
               fontSize: '24px',
               fontWeight: 'bold',
               textAlign: 'center',
               marginLeft: '25px',
+              fontFamily: 'sans-serif',
             }}
-            min='0'
             defaultValue={0}
           />
         </div>
         <div
           style={{
             borderRadius: '1rem',
-            backgroundColor: 'black',
+            backgroundColor: 'white',
             // border: '1px solid blue',
           }}
         >
           <Dropdown options={optionsThree} defaultDisplay='Select Token' />
           <input
-            type='number'
-            value={numberInputTwo}
+            type='text'
+            inputMode='decimal'
+            value={numberInputFour}
             onChange={(e) => {
               const value = e.target.value;
-              if (value >= 0) setNumberInputTwo(value);
+              // Regular expression to allow numbers and decimals
+              if (/^\d*\.?\d*$/.test(value) || value === '')
+                setNumberInputFour(value);
             }}
             style={{
-              width: '15%',
+              width: '40%',
               padding: '8px',
-              color: 'white',
-              backgroundColor: 'black',
+              color: 'black',
+              backgroundColor: 'white',
               fontSize: '24px',
               fontWeight: 'bold',
               textAlign: 'center',
               marginLeft: '25px',
+              fontFamily: 'sans-serif',
             }}
-            min='0'
             defaultValue={0}
           />
         </div>
       </div>
-      <div style={{ height: '100px' }}></div>
-      <h1 style={{ marginLeft: '250px', fontSize: '24px' }}>To</h1>
       <div
         style={{
           display: 'flex',
-          flexDirection: 'row',
-          backgroundColor: 'black',
-          padding: '16px',
-          justifyContent: 'space-around',
+          justifyContent: 'center',
           alignItems: 'center',
-          marginLeft: '250px',
-          marginRight: '250px',
-          borderRadius: '1rem',
+          height: '100px', // This assumes the container takes the full height of the screen
+          flexDirection: 'column', // to ensure other elements in this div are also centered vertically
         }}
       >
-        <div
+        <button
+          onClick={() => {
+            // Your swap logic here
+          }}
           style={{
-            borderRadius: '1rem',
-            backgroundColor: 'black',
-            // border: '1px solid blue',
+            backgroundColor: 'white',
+            color: 'black',
+            padding: '10px 20px',
+            borderRadius: '8px',
+            border: 'none',
+            cursor: 'pointer',
+            fontWeight: 'bold',
+            fontFamily: 'sans-serif',
+            boxShadow:
+              'rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset',
           }}
         >
-          <Dropdown options={optionsOne} defaultDisplay='Select Chain' />
-          <div style={{ height: '55px' }}></div>
-        </div>
-        <div
-          style={{
-            borderRadius: '1rem',
-            backgroundColor: 'black',
-            // border: '1px solid blue',
-          }}
-        >
-          <Dropdown options={optionsTwo} defaultDisplay='Select Token' />
-          <input
-            type='number'
-            value={numberInputOne}
-            onChange={(e) => {
-              const value = e.target.value;
-              if (value >= 0) setNumberInputOne(value);
-            }}
-            style={{
-              width: '15%',
-              padding: '8px',
-              color: 'white',
-              backgroundColor: 'black',
-              fontSize: '24px',
-              fontWeight: 'bold',
-              textAlign: 'center',
-              marginLeft: '25px',
-            }}
-            min='0'
-            defaultValue={0}
-          />
-        </div>
-        <div
-          style={{
-            borderRadius: '1rem',
-            backgroundColor: 'black',
-            // border: '1px solid blue',
-          }}
-        >
-          <Dropdown options={optionsThree} defaultDisplay='Select Token' />
-          <input
-            type='number'
-            value={numberInputTwo}
-            onChange={(e) => {
-              const value = e.target.value;
-              if (value >= 0) setNumberInputTwo(value);
-            }}
-            style={{
-              width: '15%',
-              padding: '8px',
-              color: 'white',
-              backgroundColor: 'black',
-              fontSize: '24px',
-              fontWeight: 'bold',
-              textAlign: 'center',
-              marginLeft: '25px',
-            }}
-            min='0'
-            defaultValue={0}
-          />
-        </div>
+          Swap
+        </button>
       </div>
-      <div style={{ height: '100px' }}></div>
     </>
   );
 }
